eb31df8ba7b4c099e4fbf4a475a0a6f2708d0597
Alysson Daniel Nascimento <alyssondaniel@gmail.com>
2019-08-24 19:57:41 -0300

    Added redirect do login in dashboard

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 import UserList from './Users';
 import PictureList from './Pictures';
+import { isAuthenticated } from "../services/auth";
+
 
 class Dashboard extends React.Component {
+  componentDidMount() {
+    if (!isAuthenticated()) {
+      return this.props.history.push('/signin');
+    }
+  }
   render() {
     return (
       <div className="content">
